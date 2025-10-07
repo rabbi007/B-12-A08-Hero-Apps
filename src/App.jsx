14e9d4af9868963ab4f57d6b93160bb6c8180bac
Khandaker Reza-e-Rabbi <40260265+rabbi007@users.noreply.github.com>
2025-10-08 04:56:04 +0600

    configured react-app, tailwindcss, daisyui, react-router, react-icons

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return <>
+  <div>
+    <h1 className="flex justify-center items-center text-6xl">B-12-A08-Hero-Apps</h1>
+  </div>
+  </>;
+};
+
+export default App;
